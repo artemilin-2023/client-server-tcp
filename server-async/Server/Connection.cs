f706f08ee100af514f532b0fdf89462507f078b3
LightChimera <artemilin347@gmail.com>
2024-02-27 20:48:18 +0300

    Реализовать асинхронную версию.

---
+++ server-async/Server/Connection.cs
@@ -0,0 +1,106 @@
+﻿using Logger.Abstracts;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+public class Connection
+{
+    private const int WorkTimeEmulated = 10000;
+    
+    internal readonly Guid Id;
+    internal readonly Socket Socket;
+    
+    private readonly ServerTcp server;
+    private readonly ILogger logger;
+    private readonly NetworkStream stream;
+
+    public Connection(Socket socket, ServerTcp server)
+    {
+        ArgumentNullException.ThrowIfNull(socket);
+        ArgumentNullException.ThrowIfNull(server);
+        
+        Id = Guid.NewGuid();
+        Socket = socket;
+        this.server = server;
+        logger = server.Logger;
+        stream = new NetworkStream(socket);
+        
+        server.AddConnections(this);
+        logger.Info($"Новое соединение: {socket.RemoteEndPoint}");
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            await TryProcessAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.Error($"При обработке клиента произошла ошибка: {ex.Message}");
+        }
+        finally
+        {
+            await server.DisconectAsync(this);
+        }
+    }
+
+    private async Task TryProcessAsync()
+    {
+        logger.Debug($"Обработка в потоке {Thread.CurrentThread.ManagedThreadId} для клиента {Socket.RemoteEndPoint}"); 
+
+        var message = await ReadAsync();
+        logger.Info($"Получено сообщение: {message} | От {Socket.RemoteEndPoint}");
+        message = Reverse(message) + " Сервер напиан Ильиным Артёмом Александровичем. М3О-107Б-23";
+        logger.Info($"Отправляю сообдщение: {message} | Кому: {Socket.RemoteEndPoint}");
+
+        Thread.Sleep(WorkTimeEmulated);
+        await SendAsync(message);
+    }
+
+    private async Task<string> ReadAsync()
+    {
+        var length = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(await ReadBytesAsync(sizeof(int)), 0));
+        return Encoding.UTF8.GetString(await ReadBytesAsync(length));
+    }
+
+    private async Task<byte[]> ReadBytesAsync(int size)
+    {
+        var buffer = new byte[size];
+        int alreadyReadCount = 0;
+
+        while (size - alreadyReadCount > 0)
+        {
+            var read = await stream.ReadAsync(buffer.AsMemory(alreadyReadCount, size - alreadyReadCount));
+            if (read == 0)
+                throw new EndOfStreamException();
+            
+            alreadyReadCount += read;
+        }
+
+        return buffer;
+    }
+
+    private string Reverse(string s)
+    {
+        var charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+    
+    // По хорошему, надо передавть данные, указывая их размер,
+    // но мне лень переделывать код клиента для грамотного считывания.
+    private async Task SendAsync(string message)
+    {
+        var messageInBytes = Encoding.UTF8.GetBytes(message);
+        await stream.WriteAsync(messageInBytes.AsMemory());
+    }
+
+    internal void Close()
+    {
+        stream.Close();
+        Socket.Close();
+    }
+}
---
+++ server-async/Server/ServerTcp.cs
@@ -0,0 +1,126 @@
+﻿using Logger.Abstracts;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Server;
+
+
+public class ServerTcp
+{
+    public bool IsRunning { get; private set; } = false;
+    
+    internal readonly ILogger Logger;
+    
+    private static ServerTcp? instance;
+    private bool isConfigured = false;
+    private readonly IPEndPoint endPoint;
+    private readonly Socket server;
+    private readonly List<Connection> connections;
+    private readonly object locker = new();
+
+    private ServerTcp(ILogger logger, IPEndPoint endPoint, Socket server)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(endPoint);
+        ArgumentNullException.ThrowIfNull(server);
+
+        Logger = logger;
+        this.endPoint = endPoint;
+        this.server = server;
+        connections = new List<Connection>();
+    }
+
+    public static ServerTcp GetOrCreate(string ip, int port, ILogger logger)
+    {
+        if (instance != null)
+            return instance;
+        
+        var endPoint = new IPEndPoint(IPAddress.Parse(ip), port); 
+        var server = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        
+        instance = new ServerTcp(logger, endPoint, server);
+        return instance;
+    }
+
+    public async Task Init()
+    {
+        if (!isConfigured)
+        {
+            server.Bind(endPoint);
+            Logger.Info($"Сервер запущен на {server.LocalEndPoint}");
+            server.Listen();
+            Logger.Info($"Сервер прослушивает сокет {server.LocalEndPoint}");
+            
+            isConfigured = true;
+        }
+        else
+        {
+            Logger.Info("Сервер уже настроен.");
+        }
+    }
+
+    public async Task RunAsync()
+    {
+        if (!isConfigured)
+            throw new Exception("The server must be configured before running");
+
+        if (IsRunning)
+        {
+            Logger.Warn("Сервер уже запущен.");
+            return;
+        }
+
+        IsRunning = true;
+        while (IsRunning)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var client = await server.AcceptAsync();
+            var connection = new Connection(client, this); // Создает объект клиента и добавляет его в коллекцию соединений
+            var process = new Thread(async () => await connection.ProcessAsync());
+            process.Start();
+            
+
+            timer.Stop();
+            Logger.Debug($"Время, затраченное на обработку клиента: {timer.Elapsed:c}");
+
+        }
+
+    }
+
+    public void Stop()
+    {
+        if (!IsRunning)
+            throw new Exception("The server is not running");
+        
+        foreach (var connection in connections)
+        {
+            connection.Close();
+        }
+        
+        server.Close();
+        IsRunning = false;
+    }
+
+    internal void AddConnections(Connection connection)
+    {
+        lock (locker)
+        {
+            connections.Add(connection);
+        }
+    }
+
+    internal async Task DisconectAsync(Connection connection)
+    {
+        Logger.Info($"Закрываю соединение с {connection.Socket.RemoteEndPoint}");
+
+        lock (locker)
+        {
+            connections.Remove(connection);
+            connection.Close();
+        }
+    }
+}
